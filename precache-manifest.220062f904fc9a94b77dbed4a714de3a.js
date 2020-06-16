fdb51bb70d35b3253baa439068ed46eafa6c84f5
jinho6225 <jinho6225@hotmail.com>
2020-06-16 00:12:00 -0700

    Updates

---
+++ precache-manifest.220062f904fc9a94b77dbed4a714de3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31998833eeffa8b4079cc2228632801e",
+    "url": "/robofriends_hooks/index.html"
+  },
+  {
+    "revision": "9ec9da31fd9412185cf4",
+    "url": "/robofriends_hooks/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5cd074a3e230cac0126e",
+    "url": "/robofriends_hooks/static/css/main.8b2cd839.chunk.css"
+  },
+  {
+    "revision": "9ec9da31fd9412185cf4",
+    "url": "/robofriends_hooks/static/js/2.bbf58502.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends_hooks/static/js/2.bbf58502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cd074a3e230cac0126e",
+    "url": "/robofriends_hooks/static/js/main.b9cba7d4.chunk.js"
+  },
+  {
+    "revision": "00d98c47d9b82eafa3af",
+    "url": "/robofriends_hooks/static/js/runtime-main.0469fa1b.js"
+  }
+]);
